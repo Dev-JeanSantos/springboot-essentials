ed8fbb4c0a161ee7187b5c07869903fa0aa9a468
Dev-JeanSantos <jeancbsan@gmail.com>
2023-08-16 16:50:05 -0300

    Enabled csrf in security app

--- src/main/java/com/academy/fourtk/springbootessentials/config/SecurityConfig.java
+++ src/main/java/com/academy/fourtk/springbootessentials/config/SecurityConfig.java
@@ -6,13 +6,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.authorizeHttpRequests()
+       http.csrf().disable()
+//               .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+               .authorizeHttpRequests()
                .anyRequest()
                .authenticated()
                .and()
