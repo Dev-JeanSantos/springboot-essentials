c2ab396d64e2dc57ed429735aa95f23c3d765296
Dev-JeanSantos <jeancbsan@gmail.com>
2023-08-16 16:26:58 -0300

    implemented security and configurated user and password autenticator

---
+++ src/main/java/com/academy/fourtk/springbootessentials/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.academy.fourtk.springbootessentials.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.authorizeHttpRequests()
+               .anyRequest()
+               .authenticated()
+               .and()
+               .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("devjs")
+                .password(passwordEncoder.encode("angela08"))
+                .roles("USER, ADMIN")
+                .and()
+                .withUser("jean")
+                .password(passwordEncoder.encode("jean"))
+                .roles("USER");
+
+    }
+}
